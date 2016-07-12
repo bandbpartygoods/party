c3a19ac70da476aea611a7a3aec9907cad34b18d
bandbpartygoods <bandbpartygoods@gmail.com>
2016-07-11 22:25:03 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
-<?php  echo '<BODY STYLE="BACKGROUND-IMAGE=LOGO.JPG"> COMING SOON </BODY>';
+<?php  echo '<h1><br />&nbsp;&nbsp;&nbsp;COMING SOON!</H1><BODY STYLE="BACKGROUND-IMAGE:url(\'logo.jpg\');background-repeat: no-repeat;background-size:100%"></BODY>';
+
+
+
